9b113b9d070e02298073affd4246d397fb2f541d
LeeTaehyung <thlee@kb-sys.com>
2022-12-26 10:23:06 +0900

    Initial Commit

---
+++ imagePagingApp/imagePagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagePagingAppApp.swift
+//  imagePagingApp
+//
+//  Created by Taehyung Lee on 2022/12/26.
+//
+
+import SwiftUI
+
+@main
+struct imagePagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
